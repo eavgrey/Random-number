9242f2a4015d6ececbc00f1e17258448cca9f3aa
eavgrey <81044186+eavgrey@users.noreply.github.com>
2022-10-18 12:43:23 +0300

    Amount of numbers

--- JS/main.js
+++ JS/main.js
@@ -6,12 +6,22 @@ let max = document.querySelector('.max')
 
 let minNum = min.value
 let maxNum = max.value
+let amountNum = amount.value
+
+function clean(){
+    result.innerHTML = ''
+}
+
 function generate(){
+    clean()
+    amountNum = amount.value
     minNum = min.value
     maxNum = max.value
-result.innerHTML = Math.floor(Math.random() * (maxNum - minNum+1)+ minNum) 
+    for(let i =0; i < amountNum; i++){
+        let random = Math.floor( Math.random() * (maxNum - minNum) + minNum) ; 
+        result.innerHTML += random + "  "; 
+    } 
 
-console.log (minNum)
 }
 
 
